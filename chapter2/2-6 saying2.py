c41c9bf4a8ad1d14a7e06664ed75ba03d07e5c67
nxh <xhniu52@outlook.com>
2021-04-22 20:53:45 +0800

    第二章第二节

---
+++ chapter2/2-6 saying2.py
@@ -0,0 +1,3 @@
+famous_person = 'albert einstein'
+message = f'{famous_person.title()} once said,"A person who never made a mistake never tried anything new."'
+print(message)
---
+++ chapter2/2-7 delete_name_blank.py
@@ -0,0 +1,5 @@
+name = '\tXinhan Niu\n'
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
